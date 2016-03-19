c42adc7bce8cd2e90dced18611b614c2745e427b
Haroen Viaene <hello@haroen.me>
2016-03-19 11:30:16 +0100

    move from foot to file

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 ---
 # comment for jekyll
 ---
+var mymap = L.map('ikhaatmap').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+maxZoom: 18,
+id: 'hyperbaton.pep3hi9n',
+accessToken: 'pk.eyJ1IjoiaHlwZXJiYXRvbiIsImEiOiJjaWx5eThiOW0wMGdudmZtNjNnNThmamQ5In0.TFkmQoeiKHGDPxct3o9Jjg'
+}).addTo(mymap);
