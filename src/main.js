c2e3e2a186b59f281b8a5301c0e1b1d3def7fc55
Haroen Viaene <hello@haroen.me>
2016-03-19 12:11:53 +0100

    things

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 ---
 # comment for jekyll
 ---
+
+// leaflet + mapbox
+
 var mymap = L.map('ikhaatmap').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -8,3 +11,19 @@ maxZoom: 18,
 id: 'hyperbaton.pep3hi9n',
 accessToken: 'pk.eyJ1IjoiaHlwZXJiYXRvbiIsImEiOiJjaWx5eThiOW0wMGdudmZtNjNnNThmamQ5In0.TFkmQoeiKHGDPxct3o9Jjg'
 }).addTo(mymap);
+
+// ugly styling of leaflet
+
+var resizeTimer;
+window.addEventListener('resize', function(){
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function() {
+    if (window.innerWidth > 960) {
+        // sidebar
+        document.getElementById('ikhaatmap').style.width = window.innerWidth - document.getElementById('menu').offsetWidth + 'px';
+    } else {
+        // over eachother
+        document.getElementById('ikhaatmap').style.height = window.innerHeight - document.getElementById('menu').offsetHeight + 'px';
+    }
+  }, 250);
+});
